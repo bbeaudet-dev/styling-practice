735dbf3d81133df0268aa8ffa901c990ce99deb8
Ben Beaudet <bbeaudet0@gmail.com>
2025-06-03 12:10:32 -0400

    Created single task (non-intial commit)

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
         <button onClick={handleCheck}
           className={clsx('aspect-square border-1 rounded-md w-5.5 h-5.5 ml-4 mr-2', checked ? 'bg-green-600 border-green-600' : 'bg-white border-gray-300')}>
         </button>
-        <div className="flex flex-col px-2 pt-2 pb-3">
+        <div className="flex flex-col px-2 pt-2 pb-3 max-w-1/2">
           <p className="text-md font-light font-inter pb-1">Sweep the Kitchen</p>
           <p className="text-xs font-extralight text-gray-500">Get under the cabinets, do a good job</p>
         </div>
